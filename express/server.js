19c216e8da9b06e7f76f23507700fefeafd6eca3
Gladden Rumao <gladdenbackup@gmail.com>
2025-05-19 18:54:15 +0530

    Added Rest API - HTTP methods

--- express/server.js
+++ express/server.js
@@ -13,6 +13,7 @@ let products = [
 app.get('/products', (req,res)=>{
     res.json(products);
 })
+
 // GET Product by ID
 app.get('/products/:id', (req,res)=>{
     const id = parseInt(req.params.id);
@@ -41,6 +42,36 @@ app.post('/products',(req,res)=>{
     products.push(newProduct);
     res.status(201).json(newProduct);
 })
+
+// PUT - Update a product
+app.put('/products/:id', (req,res)=>{
+    const id = parseInt(req.params.id);
+    const {name} = req.body;
+    const product = products.find(p => p.id === id);
+
+    if(!product){
+        return res.status(404).json({message:'Product not found'})
+    }
+
+    if(!name){
+        return res.status(400).json({message:'Product name is required'})
+    }
+
+    product.name = name;
+    res.json(product);
+})
+
+// DELETE - Remove a product
+app.delete('/products/:id', (req,res)=>{
+    const id = parseInt(req.params.id);
+    const index = products.findIndex(p=>p.id===id)
+
+    if(index===-1){
+        return res.status(404).json({message:'Product not found'})
+    }
+    products.splice(index,1);
+    res.json(({message:'Product deleted successfully.'}));
+});
 
 
 app.listen(3002,()=>{
