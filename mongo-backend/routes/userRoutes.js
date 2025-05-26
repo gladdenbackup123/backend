0c9ca2eafc9ea1fe2f591fad92c9f20057f49fc1
Gladden Rumao <gladdenbackup@gmail.com>
2025-05-26 19:16:44 +0530

    Updated CRUD Operations

--- mongo-backend/routes/userRoutes.js
+++ mongo-backend/routes/userRoutes.js
@@ -8,4 +8,34 @@ router.post('/', async (req,res)=>{
     res.status(201).json(user);
 })
 
+// Read all users
+router.get('/',async (req,res)=>{
+    const users = await User.find();
+    res.json(users);
+})
+
+// Read a single user by ID 
+router.get('/:id',async (req,res)=>{
+    const user = await User.findById(req.params.id);
+    res.json(user);
+})
+
+// Update a user by ID
+router.put('/:id',async (req,res)=>{
+    userId = req.params.id.trim();
+    const updatedUser = await User.findByIdAndUpdate(
+        userId,
+        req.body,
+        {new : true}
+    )
+
+    res.json(updatedUser);
+})
+
+// Delete a user by ID 
+router.delete('/:id',async (req,res)=>{
+    await User.findByIdAndDelete(req.params.id);
+    res.json({message:"User deleted"})
+})
+
 module.exports = router;
